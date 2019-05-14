86f9300bf396c0755f7bef8edcb922d93f2600d0
Yu Li <yu.li@tecsys.com>
2019-05-14 13:09:39 -0400

    Initial commit.

---
+++ src/main/java/com/yulikexuan/security/jwtlab/JwtlabApplication.java
@@ -0,0 +1,18 @@
+//: com.yulikexuan.security.jwtlab.JwtlabApplication.java
+
+
+package com.yulikexuan.security.jwtlab;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JwtlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtlabApplication.class, args);
+    }
+
+}///:~
---
+++ src/test/java/com/yulikexuan/security/jwtlab/JwtlabApplicationIT.java
@@ -0,0 +1,21 @@
+//: com.yulikexuan.security.jwtlab.JwtlabApplicationIT.java
+
+
+package com.yulikexuan.security.jwtlab;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwtlabApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}///:~
