1e1d8ab94247d1f6e8f74a011b14c6aa5ae279c4
Yu Li <yu.li@tecsys.com>
2019-05-28 16:56:50 -0400

    remove .idea

---
+++ jwt-security/src/test/java/com/yulikexuan/security/securitylab/SecuritylabApplicationIT.java
@@ -0,0 +1,15 @@
+package com.yulikexuan.security.securitylab;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SecuritylabApplicationIT {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
