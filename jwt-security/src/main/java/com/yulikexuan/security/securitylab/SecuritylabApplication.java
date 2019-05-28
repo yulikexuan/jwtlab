2cc3cd0a040010d6285f421ab0eaa1fb1af15c0a
Yu Li <yu.li@tecsys.com>
2019-05-28 16:18:34 -0400

    Add new module for standard security driven by JWT

---
+++ jwt-security/src/main/java/com/yulikexuan/security/securitylab/SecuritylabApplication.java
@@ -0,0 +1,13 @@
+package com.yulikexuan.security.securitylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritylabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritylabApplication.class, args);
+    }
+
+}
---
+++ jwt-security/src/test/java/com/yulikexuan/security/securitylab/SecuritylabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yulikexuan.security.securitylab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecuritylabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
